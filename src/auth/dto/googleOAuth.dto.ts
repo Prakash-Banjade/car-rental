eddd08c4875b992251abf7e81db76c72673cba7c
Prakash Banjade <prakashbanjade191@gmail.com>
2024-08-02 21:52:35 +0545

    use id_token instead of code

--- src/auth/dto/googleOAuth.dto.ts
+++ src/auth/dto/googleOAuth.dto.ts
@@ -1,7 +1,7 @@
-import { IsNotEmpty, IsOptional, IsString } from "class-validator";
+import { IsNotEmpty, IsString } from "class-validator";
 
 export class GoogleOAuthDto {
     @IsString()
     @IsNotEmpty()
-    code: string
+    id_token: string;
 }
