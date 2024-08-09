262deccdf109b4c50d95b8137ec409a53cfef01a
Prakash Banjade <prakashbanjade191@gmail.com>
2024-08-09 12:00:14 +0545

    update cors origin

--- src/main.ts
+++ src/main.ts
@@ -12,7 +12,13 @@ const PORT = process.env.PORT || 3000;
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: ['http://localhost:3000', 'http://localhost:3001'],
+    origin: (origin: string, callback: Function) => {
+      if (origin === process.env.CLIENT_URL || origin === process.env.CMS_URL || (process.env.NODE_ENV === 'development' && !origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     credentials: true,
     optionsSuccessStatus: 200,
     preflightContinue: false, // enforce CORS policy consistently across the application's endpoints.
